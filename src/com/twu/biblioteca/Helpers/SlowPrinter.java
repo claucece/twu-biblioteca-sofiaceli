4496394fe2f49c1324563b1dc3ab1b08e1229d2d
Sofia Celi <sceli@thoughtworks.com>
2016-03-31 12:29:48 -0500

    save thread

--- src/com/twu/biblioteca/Helpers/SlowPrinter.java
+++ src/com/twu/biblioteca/Helpers/SlowPrinter.java
@@ -2,13 +2,27 @@ package com.twu.biblioteca.helpers;
 
 public class SlowPrinter {
 
+    private static boolean stopPrinter;
+
+    private static synchronized void requestStop() {
+        stopPrinter = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopPrinter;
+    }
+
     public void printSlowly(String message, long millisPerChar) {
+        while (!stopRequested()) {
         for (int i = 0; i < message.length(); i++) {
-            System.out.print(message.charAt(i));
-            try {
-                Thread.sleep(millisPerChar);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.out.print(message.charAt(i));
+                try {
+                    Thread.sleep(millisPerChar);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    requestStop();
+                }
             }
         }
     }
