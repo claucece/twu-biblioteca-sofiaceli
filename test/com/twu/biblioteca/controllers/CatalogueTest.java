efa643b0cccb4ccbd5e26a65dc80376c8f94d3dd
Sofia Celi <sceli@thoughtworks.com>
2016-04-04 17:05:12 -0500

    catalogue tests refactored and a bug found

--- test/com/twu/biblioteca/controllers/CatalogueTest.java
+++ test/com/twu/biblioteca/controllers/CatalogueTest.java
@@ -4,6 +4,9 @@ import com.twu.biblioteca.helpers.Element;
 import com.twu.biblioteca.models.BookModel.BookInventory;
 import com.twu.biblioteca.models.BookModel.Book;
 import com.twu.biblioteca.models.BookModel.BookSpec;
+import com.twu.biblioteca.models.MovieModel.Movie;
+import com.twu.biblioteca.models.MovieModel.MovieInventory;
+import com.twu.biblioteca.models.MovieModel.MovieSpec;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,12 +17,16 @@ import static org.junit.Assert.*;
 public class CatalogueTest {
 
     private BookInventory bookInventory;
-    private Catalogue catalogue;
+    private MovieInventory movieInventory;
+    private Catalogue bookCatalogue;
+    private Catalogue movieCatalogue;
 
     @Before
     public void setUp() {
         bookInventory = new BookInventory();
-        catalogue = new Catalogue(bookInventory);
+        movieInventory = new MovieInventory();
+        bookCatalogue = new Catalogue(bookInventory);
+        movieCatalogue = new Catalogue(movieInventory);
     }
 
     @Test
@@ -28,47 +35,108 @@ public class CatalogueTest {
                 "demian                  Herman Hesse     1980 FICTION NOVEL       \n" +
                 "moby dick               Herman Melville  1980 FICTION SHORT_STORY " +
                 "\n";
-        String actual = catalogue.putInformationInColumns(bookInventory);
+        String actual = bookCatalogue.putInformationInColumns(bookInventory);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMovieInformation() {
+        String expected = "lola rennt           Tom Tykwer  1998 Independant Suspense \n" +
+                "being john malkovich Spike Jonze 1999 Independant Suspense \n" +
+                "blue velvet          David Lynch 1986 Independant Suspense " +
+                "\n";
+        String actual = movieCatalogue.putInformationInColumns(movieInventory);
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldBeAbleToCheckoutBooks() {
-        boolean actual = catalogue.isACheckout("Moby Dick", bookInventory);
+        boolean actual = bookCatalogue.isACheckout("Moby Dick", bookInventory);
         assertEquals(true, actual);
     }
 
     @Test
+    public void shouldBeAbleToCheckoutMovies() {
+        boolean actual = bookCatalogue.isACheckout("Lola Rennt", movieInventory);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void shouldNotBeAbleToCheckoutMovieThatIsNotOnBookInventory() {
+        boolean actual = bookCatalogue.isACheckout("Lola Rennt", bookInventory);
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void shouldNotBeAbleToCheckoutBookThatIsNotOnMovieInventory() {
+        boolean actual = movieCatalogue.isACheckout("Moby Dick", movieInventory);
+        assertEquals(false, actual);
+    }
+
+    @Test
     public void shouldNotBeAbleToCheckoutBookThatIsNotOnInventory() {
-        boolean actual = catalogue.isACheckout("The Art Of Love", bookInventory);
+        boolean actual = bookCatalogue.isACheckout("The Art Of Love", bookInventory);
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void shouldNotBeAbleToCheckoutMovieThatIsNotOnInventory() {
+        boolean actual = bookCatalogue.isACheckout("Dodgeball", movieInventory);
         assertEquals(false, actual);
     }
 
     @Test
     public void shouldRemoveCheckoutBookFromInventory() {
         Book book = new Book(new BookSpec("Moby Dick", "Herman Melville", "1980", BookSpec.Genre.Type.FICTION, BookSpec.Genre.NOVEL));
-        boolean bookList = catalogue.removeFromInventory(book, bookInventory);
+        boolean bookList = bookCatalogue.removeFromInventory(book, bookInventory);
         assertFalse(bookInventory.returnInventoryOfElements().contains(book));
     }
 
     @Test
+    public void shouldRemoveCheckoutMovieFromInventory() {
+        Movie movie = new Movie(new MovieSpec("Titanic", "James Cameron", "1997", "10", "FICTION", "ROMANTIC"));
+        boolean movieList = movieCatalogue.removeFromInventory(movie, movieInventory);
+        assertFalse(movieInventory.returnInventoryOfElements().contains(movie));
+    }
+
+    @Test
     public void shouldBeAbleToReturnBooks() {
-        catalogue.isACheckout("Moby Dick", bookInventory);
-        boolean actual = catalogue.isAReturn("Moby Dick", bookInventory);
+        bookCatalogue.isACheckout("Moby Dick", bookInventory);
+        boolean actual = bookCatalogue.isAReturn("Moby Dick", bookInventory);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void shouldBeAbleToReturnMovies() {
+        movieCatalogue.isACheckout("Lola Rennt", movieInventory);
+        boolean actual = movieCatalogue.isAReturn("Lola Rennt", movieInventory);
         assertEquals(true, actual);
     }
 
     @Test
     public void shouldNotBeAbleToReturnBookThatIsNotOnList() {
-        boolean actual = catalogue.isAReturn("Hamlet", bookInventory);
+        boolean actual = bookCatalogue.isAReturn("Hamlet", bookInventory);
+        assertEquals(false, actual);
+    }
+
+    // and this is a bug
+    @Test
+    public void shouldNotBeAbleToReturnMovieThatIsNotOnList() {
+        boolean actual = movieCatalogue.isAReturn("Eraser Head", movieInventory);
+        assertEquals(false, actual);
+    }
+
+    @Test
+    public void shouldNotBeAbleToReturnMovieThatIsOnBookList() {
+        boolean actual = movieCatalogue.isAReturn("Moby Dick", movieInventory);
         assertEquals(false, actual);
     }
 
     @Test
     public void shouldAddCheckoutBookFromInventory() {
         List<Element> startList = bookInventory.returnInventoryOfElements();
-        catalogue.isACheckout("Moby Dick", bookInventory);
-        catalogue.isAReturn("Moby Dick", bookInventory);
+        bookCatalogue.isACheckout("Moby Dick", bookInventory);
+        bookCatalogue.isAReturn("Moby Dick", bookInventory);
         List<Element> finishList = bookInventory.returnInventoryOfElements();
         assertEquals(startList.size(), finishList.size());
     }
