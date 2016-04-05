a82d1cc0f76bf1f2fe972827b3d57bbd1da66044
Sofia Celi <sceli@thoughtworks.com>
2016-04-05 00:12:50 -0500

    some tests still failing

--- test/com/twu/biblioteca/views/MenuCatalogueTest.java
+++ test/com/twu/biblioteca/views/MenuCatalogueTest.java
@@ -1,33 +1,33 @@
 package com.twu.biblioteca.views;
 
+import com.twu.biblioteca.controllers.Catalogue;
+import com.twu.biblioteca.models.Inventory;
+import com.twu.biblioteca.models.bookModel.BookInventory;
+import com.twu.biblioteca.models.movieModel.MovieInventory;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class MenuCatalogueTest {
 
     private MenuCatalogue menuCatalogue;
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private BookInventory bookInventory = new BookInventory();
 
-//    @Before
-//    public void setUp() {
-//        menuCatalogue = new MenuCatalogue();
-//    }
-//
-//    @Test
-//    public void shouldReturnBookInformation() {
-//        String expected = "for whom the bells toll Ernest Hemingway 1980 FICTION NOVEL       \n" +
-//                "demian                  Herman Hesse     1980 FICTION NOVEL       \n" +
-//                "moby dick               Herman Melville  1980 FICTION SHORT_STORY " +
-//                "\n";
-//        String actual = menuCatalogue.returnCatalogue();
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldPrintMenuItems() {
-//        System.setOut(new PrintStream(outContent));
-//        String actual = "--> Checkout Book   --> Return Book   --> Return To Main Menu   \n";
-//        assertEquals(menuCatalogue.toLineColumn(), actual);
-//    }
+    @Before
+    public void setUp() {
+        menuCatalogue = new MenuCatalogue();
+    }
 
+    ///not passing
+    @Test
+    public void shouldPrintMenuItems() {
+        String actual = "--> Checkout Book   --> Return Book   --> Return To Main Menu   \n";
+        assertEquals(menuCatalogue.toLineColumn(bookInventory), actual);
+    }
 
 }
