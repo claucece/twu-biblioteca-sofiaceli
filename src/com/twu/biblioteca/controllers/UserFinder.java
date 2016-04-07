db9c43c128725b1c42528c2c67b333af2dc768db
Sofia Celi <sceli@thoughtworks.com>
2016-04-07 15:28:32 -0500

    controller for user sessions now created

---
+++ src/com/twu/biblioteca/controllers/UserFinder.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca.controllers;
+
+/**
+ * Created by sceli on 4/7/16.
+ */
+public class UserFinder {
+}
---
+++ test/com/twu/biblioteca/controllers/UserFinderTest.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca.controllers;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sceli on 4/7/16.
+ */
+public class UserFinderTest {
+
+}
